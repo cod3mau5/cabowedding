1d128a412c03440e6262ee16706992f5eda8009b
cod3mau5 <code.bit.mau@gmail.com>
2022-12-30 13:34:17 -0700

    new video home

--- wp-content/themes/cabowedding/front-page.php
+++ wp-content/themes/cabowedding/front-page.php
@@ -7,7 +7,7 @@
 
         <!-- main video -->
         <div style="padding:44.79% 0 0 0;position:relative;">
-            <iframe src="https://player.vimeo.com/video/527901037?h=baf17df27f&autoplay=1&title=0&byline=0&portrait=0" 
+            <iframe src="https://player.vimeo.com/video/785148596?h=9243543f11&autoplay=1&title=0&byline=0&portrait=0" 
                 style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" 
                 allow="autoplay; fullscreen; picture-in-picture" width="100%" height="500px"
                  frameborder="0"
