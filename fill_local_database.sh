19b2f313207f1932e200694be2a5f9ec7699bc63
cod3mau5 <code.bit.mau@gmail.com>
2023-05-30 15:14:57 -0700

    bash scripts to migrate wordpress db

---
+++ fill_local_database.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Variables
+DB_USER="u606769855_cabowedding"
+DB_PASSWORD="Red_hat_4944"
+DB_NAME="u606769855_cabowedding"
+DUMP_DIR="sql_dumps/"
+
+# Obtener el nombre del archivo de volcado más reciente
+DUMP_FILE=$(ls -t $DUMP_DIR/*.sql | head -n 1)
+
+# Importar el volcado a la base de datos local
+mysql -u $DB_USER -p$DB_PASSWORD $DB_NAME < $DUMP_FILE
+
+# Reemplazar el nombre del dominio
+wp --allow-root search-replace 'http://caboweddingfilms.com' 'http://cabowedding.local' --skip-columns=guid
